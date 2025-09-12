19f02f469f7dafe226b21fa5d9ecb32c56cd446e
AnnaHrulkova <annahrulkova@yandex.ru>
2025-09-12 23:10:25 +0300

    init

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+ import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
+import importPlugin from 'eslint-plugin-import'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      prettier,
+      import: importPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal'],
+            ['parent', 'sibling', 'index'],
+          ],
+          'newlines-between': 'always',
+        },
+      ],
+    },
+  },
+])
