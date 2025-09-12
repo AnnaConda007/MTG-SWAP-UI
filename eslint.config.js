fde79e102ecfeb08a223c0da46a411e50607fd36
AnnaHrulkova <annahrulkova@yandex.ru>
2025-09-13 00:02:27 +0300

    added storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,44 @@
- import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-import prettier from 'eslint-plugin-prettier'
-import importPlugin from 'eslint-plugin-import'
+import storybook from 'eslint-plugin-storybook';
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      prettier,
-      import: importPlugin,
-    },
-    rules: {
-      'prettier/prettier': 'warn',
-      'import/order': [
-        'warn',
-        {
-          groups: [
-            ['builtin', 'external'],
-            ['internal'],
-            ['parent', 'sibling', 'index'],
-          ],
-          'newlines-between': 'always',
-        },
+export default tseslint.config(
+  [
+    globalIgnores(['dist']),
+    {
+      files: ['**/*.{ts,tsx}'],
+      extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        reactHooks.configs['recommended-latest'],
+        reactRefresh.configs.vite,
       ],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+      },
+      plugins: {
+        prettier,
+        import: importPlugin,
+      },
+      rules: {
+        'prettier/prettier': ['warn', { singleQuote: true }],
+        quotes: ['error', 'single'],
+        'no-duplicate-imports': 'error',
+        'import/order': [
+          'warn',
+          {
+            'newlines-between': 'never', // 👈 убираем пустые строки между группами
+          },
+        ],
+      },
     },
-  },
-])
+  ],
+  storybook.configs['flat/recommended'],
+);
---
+++ src/shared/ui-kit/stories/atoms/Button.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { Button } from '../../atoms/button';
+
+const meta = {
+  title: 'Atoms/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
